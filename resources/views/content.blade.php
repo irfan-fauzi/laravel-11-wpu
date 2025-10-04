bd18cbe64e318791375142b8a6bf17b9316a47ac
= <=>
2025-10-04 13:23:14 +0700

    yeah

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
   <x-slot:header>{{ $header }}</x-slot:header>
-  <h1>Home</h1>
+  <h1>Contact</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita, exercitationem?</p>
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea distinctio libero odit, sequi hic accusantium doloribus, voluptas esse exercitationem cumque aliquam! Itaque expedita voluptatum quaerat officia non facere eum rem.</p>
 </x-layout>
